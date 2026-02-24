1ce05f8a9ed4a298758da39d00d31ca618008651
MariyaMathew <mariyapoozhikunnel5@gmail.com>
2026-02-23 22:45:32 -0800

    Added Amazon Schema Parser

--- Program.cs
+++ Program.cs
@@ -1,41 +1,43 @@
+using QuiptMappingEngine.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+//creating a browser endpoint
+app.MapGet("/", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var parser = new AmazonFieldParser();
+    var filePath = "AmazonTaxonomy/amazon-laptops-attributes.json";
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    if (!File.Exists(filePath))
+    {
+        return Results.Text($"File NOT found at: {filePath}");
+    }
+
+    try
+    {
+        var fields = parser.Parse(filePath);
+
+        var result = "";
+
+        foreach (var f in fields)
+        {
+            result += $"Name: {f.Name}\n";
+            result += $"Path: {f.Path}\n";
+            result += $"Type: {f.DataType}\n";
+            result += $"Required: {f.IsRequired}\n";
+            result += "-------------------------\n";
+        }
+
+        return Results.Text(result);
+    }
+    catch (Exception ex)
+    {
+        return Results.Text($"ERROR:\n{ex.Message}");
+    }
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
